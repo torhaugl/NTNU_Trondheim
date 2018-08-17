c9910a7f06b8d73c4c5fcfae74b0faed35711650
Tor Strømsem Haugland <torhaugl@stud.ntnu.no>
2018-08-17 11:51:40 +0200

    Makes directory and plots

--- fortran/run.sh
+++ fortran/run.sh
@@ -1,3 +1,17 @@
 gfortran csv_file.f90 simulate.f90 -O3
-rm *.mod *.o
-./a.out
+rm *.mod
+directory=$(date '+%y-%m-%d-')$1
+mkdir data/${directory}
+mv a.out data/${directory}/
+cp data/plot.gnuplot data/${directory}/
+cp data/sum.sh data/${directory}/
+cd data/$directory
+mkdir data
+./a.out &&
+cp sum.sh data/
+cp plot.gnuplot data/
+cd data
+./sum.sh &&
+./plot.gnuplot
+mv *.png ../
+cd ../../..
